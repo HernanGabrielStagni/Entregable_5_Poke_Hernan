8f038edfefd8c403a96c1419de942d2876516864
HernanGabrielStagni <psicologohernanstagni@gmail.com>
2023-06-02 00:21:28 -0300

    agregue fondo al input

--- src/components/Poke_Info_folder/ProgressBar.jsx
+++ src/components/Poke_Info_folder/ProgressBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Styles/CSS_ProgressBar.css'
 const ProgressBar = ({progress}) => {
 
   const Parent__container  = {
@@ -7,12 +7,12 @@ const ProgressBar = ({progress}) => {
 		height: '23px',
 		width: '100%',
 		backgroundColor: '#fcefef',
-		borderRadius: 40,
+		borderRadius: 0,
 		marginTop: 10,
 		marginBottom: 10,
 		marginLeft:0,
 		marginRight: 0,
-    padding: 1
+    padding:  1
 	}
 
   const Child__progress = {
@@ -20,13 +20,23 @@ const ProgressBar = ({progress}) => {
 		width: `${progress}%`,
 		// maxWidth: '100%',
 		background: 'linear-gradient(90deg, #FCD676 -2.25%, #E6901E 133.18%)',
-	    borderRadius: 5,
-		textAlign: 'right'
+	    borderRadius: 3,
+		textAlign: 'right',
+    display:'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
 	}
+
     return (
     <div style={Parent__container}>
       <div style={Child__progress}>
-        ProgressBar
+      <div className="progress__bar-text">
+         {/* <div className='progress__bar-text' > */}
+         ProgressBar
+           ${progress}%
+         
+         </div>
+      
       </div>
      
     </div>
